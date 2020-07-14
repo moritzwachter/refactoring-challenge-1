5d80ea9042f869d383fe74f70d5074b034732c4b
moritz.wachter <moritz.wachter@exozet.com>
2020-07-14 08:53:40 +0200

    added test for error case + moved HOST to const

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -6,17 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    const HOST = 'http://refactoring.localtest.me';
+
     public function testIfReadmeRequestsAreWorking()
     {
-        $host = 'http://refactoring.localtest.me';
         $client = static::createClient();
-        $client->request('GET', $host . '/?file=video_12345_q8c.mp4&directory=video1&quality=q6a&type=video');
+        $client->request('GET', self::HOST . '/?file=video_12345_q8c.mp4&directory=video1&quality=q6a&type=video');
 
         $response = $client->getResponse();
         $this->assertSame(200, $response->getStatusCode());
 
-        $client->request('GET', $host . '/?file=image_12345.png&directory=image2&type=image');
+        $client->request('GET', self::HOST . '/?file=image_12345.png&directory=image2&type=image');
         $response = $client->getResponse();
         $this->assertSame(200, $response->getStatusCode());
+    }
+
+    public function testNonExistingTypeParameter()
+    {
+        $client = static::createClient();
+        $client->request('GET', self::HOST . '/?file=video_12345_q8c.mp4&directory=video1&quality=q6a&type=audio');
+
+        $response = $client->getResponse();
+        $this->assertSame(500, $response->getStatusCode());
+        $this->assertSame(['status' => 'error'], $response->getContent());
     }
 }
