e3491e692ee59092984aeaf3583a46548efd571c
moritz.wachter <moritz.wachter@exozet.com>
2020-07-17 23:47:00 +0200

    added test cases for id parsing

--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -30,4 +30,73 @@ class DefaultControllerTest extends WebTestCase
         $this->assertJsonStringEqualsJsonString('{"status": "error"}', $response->getContent());
         $this->assertSame(500, $response->getStatusCode());
     }
+
+    /**
+     * @dataProvider parsingImageIdDataProvider
+     *
+     * @param $filename
+     * @param $expectToFindEntity
+     */
+    public function testParsingImageId($filename, $expectToFindEntity)
+    {
+        $url = self::HOST . "/?file=${filename}&directory=irrelevant&quality=irrelevant&type=image";
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $response = $client->getResponse();
+
+        if ($expectToFindEntity) {
+            $this->assertSame(200, $response->getStatusCode());
+        } else {
+            $this->assertSame(500, $response->getStatusCode());
+        }
+    }
+
+    public function parsingImageIdDataProvider(): array
+    {
+        return [
+            ['image_12345.irrelevant', true],
+            ['image_12346.gif', true],
+            ['image-something_12347.mp4', true],
+            ['image_12348.jpg', false],
+            ['image_12349.jpg', false],
+            ['image12349.jpg', false],
+            ['im_age_12345.jpg', false],
+        ];
+    }
+
+    /**
+     * @dataProvider parsingVideoIdDataProvider
+     *
+     * @param $filename
+     * @param $expectToFindEntity
+     */
+    public function testParsingVideoId($filename, $quality, $expectToFindEntity)
+    {
+        $url = self::HOST . "/?file=${filename}&directory=irrelevant&quality=${quality}&type=video";
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $response = $client->getResponse();
+
+        if ($expectToFindEntity) {
+            $this->assertSame(200, $response->getStatusCode());
+        } else {
+            $this->assertSame(500, $response->getStatusCode());
+        }
+    }
+
+    public function parsingVideoIdDataProvider(): array
+    {
+        return [
+            ['video_12345_Q6A.irrelevant', 'q8c', true],
+            ['vid-eo_12346_ASDF.irrelevant', 'q6a', true],
+            ['_12347_ThisIsCompletely.irrelevant', 'q4a', true],
+            ['_12347_ThisIsCompletely.irrelevant', 'Q4A', false],
+            ['video_12347.mp4', 'q8c', false],
+            ['video_12348_Q8C.mp4', 'q8c', false],
+            ['video12349.jpg', 'q8c', false],
+            ['vi_deo_12345.jpg', 'q8c', false],
+        ];
+    }
 }
