f57c14ad2858ae5ecb5f6ed9653008792bdc469a
moritz.wachter <moritz.wachter@exozet.com>
2020-07-26 23:21:29 +0200

    extract behaviour into separate methods

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -27,35 +27,9 @@ class DefaultController
         $success = false;
         try {
             if ($type === 'image') {
-                $logger->debug('Request is of type: image.');
-
-                $firstUnderscore = strpos($filename, '_');
-                $sU = strpos($filename, '.', $firstUnderscore + 1);
-                $id = (int) substr($filename, $firstUnderscore + 1, $sU - $firstUnderscore - 1);
-                $image = $repo->getImageById($id);
-
-                $image->setDirectory($directory);
-                $image->setIsDistributed(true);
-                $repo->save($image);
-
-                $success = true;
+                $success = $this->handleImage($logger, $filename, $repo, $directory);
             } elseif ($type === 'video') {
-                $logger->debug('Request is of type: video.');
-
-                $firstUnderscore = strpos($filename, '_');
-                $secondUnderscore = strpos($filename, '_', $firstUnderscore + 1);
-                $id = (int) substr($filename, $firstUnderscore + 1, $secondUnderscore - $firstUnderscore - 1);
-
-                $video = $repo->getVideoById($id);
-
-                $video->setDirectory($directory);
-                $video->setIsDistributed(true);
-
-                $quality = $repo->getQualityByKey($qualityKey);
-                $video->setQuality($quality);
-                $repo->save($video);
-
-                $success = true;
+                $success = $this->handleVideo($logger, $filename, $repo, $directory, $qualityKey);
             }
         } catch (EntityNotFoundException $notFoundHttpException) {
             return new JsonResponse(['status' => 'not found'], 404);
@@ -66,5 +40,58 @@ class DefaultController
         }
 
         return new JsonResponse(['status' => 'error'], 500);
+    }
+
+    /**
+     * @param LoggerInterface $logger
+     * @param $filename
+     * @param FakeRepository $repo
+     * @param $directory
+     * @return bool
+     * @throws EntityNotFoundException
+     */
+    public function handleImage(LoggerInterface $logger, $filename, FakeRepository $repo, $directory): bool
+    {
+        $logger->debug('Request is of type: image.');
+
+        $firstUnderscore = strpos($filename, '_');
+        $sU = strpos($filename, '.', $firstUnderscore + 1);
+        $id = (int)substr($filename, $firstUnderscore + 1, $sU - $firstUnderscore - 1);
+        $image = $repo->getImageById($id);
+
+        $image->setDirectory($directory);
+        $image->setIsDistributed(true);
+        $repo->save($image);
+
+        return true;
+    }
+
+    /**
+     * @param LoggerInterface $logger
+     * @param $filename
+     * @param FakeRepository $repo
+     * @param $directory
+     * @param $qualityKey
+     * @return bool
+     * @throws EntityNotFoundException
+     */
+    public function handleVideo(LoggerInterface $logger, $filename, FakeRepository $repo, $directory, $qualityKey): bool
+    {
+        $logger->debug('Request is of type: video.');
+
+        $firstUnderscore = strpos($filename, '_');
+        $secondUnderscore = strpos($filename, '_', $firstUnderscore + 1);
+        $id = (int)substr($filename, $firstUnderscore + 1, $secondUnderscore - $firstUnderscore - 1);
+
+        $video = $repo->getVideoById($id);
+
+        $video->setDirectory($directory);
+        $video->setIsDistributed(true);
+
+        $quality = $repo->getQualityByKey($qualityKey);
+        $video->setQuality($quality);
+        $repo->save($video);
+
+        return true;
     }
 }
