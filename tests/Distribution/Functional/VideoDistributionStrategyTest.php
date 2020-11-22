2cd144a2dd9d3f07bb35b332e47ca8840bd718d3
moritz.wachter <moritz.wachter@exozet.com>
2020-11-22 12:41:40 +0100

    added missing functional test

--- tests/Distribution/Functional/VideoDistributionStrategyTest.php
+++ tests/Distribution/Functional/VideoDistributionStrategyTest.php
@@ -4,16 +4,16 @@ namespace App\Tests\Distribution\Functional;
 
 use App\Distribution\VideoDistributionStrategy;
 use App\DTO\DistributionDTO;
+use App\Mocks\FakeDatabase;
 use App\Mocks\FakeRepository;
 use App\Mocks\Quality;
-use App\Mocks\Video;
 use Doctrine\ORM\EntityNotFoundException;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
 
 class VideoDistributionStrategyTest extends TestCase
 {
-    /** @var FakeRepository|\PHPUnit\Framework\MockObject\MockObject */
+    /** @var FakeRepository */
     private $repo;
 
     /** @var VideoDistributionStrategy */
@@ -21,50 +21,29 @@ class VideoDistributionStrategyTest extends TestCase
 
     public function setUp(): void
     {
-        $this->repo = $this->createMock(FakeRepository::class);
+        $this->repo = new FakeRepository(new FakeDatabase());
         $this->strategy = new VideoDistributionStrategy($this->repo, new NullLogger());
     }
 
     public function testDistributeFile()
     {
-        $videoMock = $this->createMock(Video::class);
-        $videoMock
-            ->expects($this->once())
-            ->method('setDirectory')
-            ->with('directory');
-
-        $quality = new Quality('q8c', 'sehr hoch');
-        $this->repo->expects($this->once())
-            ->method('getQualityByKey')
-            ->with('q8c')
-            ->willReturn($quality);
-        $videoMock->expects($this->once())
-            ->method('setQuality')
-            ->with($quality);
-
-        $videoMock->expects($this->once())
-            ->method('setIsDistributed')
-            ->with(true);
-
-        $this->repo
-            ->expects($this->once())
-            ->method('getVideoById')
-            ->with('12345')
-            ->willReturn($videoMock);
-        $this->repo->expects($this->once())->method('save');
+        // check if it's not distributed before calling the method
+        $imageBefore = $this->repo->getVideoById('12345');
+        $this->assertFalse($imageBefore->isDistributed());
 
         $dto = new DistributionDTO('video_12345_q8c.mp4', 'directory', 'video', 'q8c');
         $this->strategy->handleDistribution($dto);
+
+        // check if it's now distributed
+        $video = $this->repo->getVideoById('12345');
+        $this->assertSame('directory', $video->getDirectory());
+        $this->assertTrue($video->isDistributed());
+        $this->assertEquals(new Quality('q8c', 'sehr hoch'), $video->getQuality());
     }
 
     public function testEntityNotFoundException()
     {
         $dto = new DistributionDTO('video12345.jpg', 'directory', 'video', '');
-        $this->repo
-            ->expects($this->once())
-            ->method('getVideoById')
-            ->with('0')->willThrowException(new EntityNotFoundException())
-        ;
         $this->expectException(EntityNotFoundException::class);
 
         $this->strategy->handleDistribution($dto);
